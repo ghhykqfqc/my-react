0ade8db67cc609c91222bbaa5405f3c0fc34d6e4
HYongkang <hyk8899@mail.notes.bank-of-china.com>
2023-11-29 13:49:41 +0800

    修改导航菜单适配多级

--- src/App.js
+++ src/App.js
@@ -5,11 +5,44 @@ import {
   UploadOutlined,
   UserOutlined,
   VideoCameraOutlined,
+  AppstoreOutlined,
+  MailOutlined,
+  SettingOutlined
 } from '@ant-design/icons';
 import { Layout, Menu, Button, theme } from 'antd';
 
 
 const { Header, Sider, Content } = Layout;
+function getItem(label, key, icon, children, type) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
+}
+
+const items = [
+  getItem('Navigation One', 'sub1', <MailOutlined />, [
+    getItem('Option 1', '1'),
+    getItem('Option 2', '2'),
+    getItem('Option 3', '3'),
+    getItem('Option 4', '4'),
+  ]),
+  getItem('Navigation Two', 'sub2', <AppstoreOutlined />, [
+    getItem('Option 5', '5'),
+    getItem('Option 6', '6'),
+    getItem('Submenu', 'sub3', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
+  ]),
+  getItem('Navigation Three', 'sub4', <SettingOutlined />, [
+    getItem('Option 9', '9'),
+    getItem('Option 10', '10'),
+    getItem('Option 11', '11'),
+    getItem('Option 12', '12'),
+  ]),
+];
+
 const App = () => {
   const [collapsed, setCollapsed] = useState(false);
   const {
@@ -23,23 +56,7 @@ const App = () => {
           theme="dark"
           mode="inline"
           defaultSelectedKeys={['1']}
-          items={[
-            {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'nav 1',
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
-            },
-            {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3',
-            },
-          ]}
+          items={items}
         />
       </Sider>
       <Layout>
