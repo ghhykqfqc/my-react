f85d7f93e5b1e4cd5a544c3d03afed0c86ce52a6
HYongkang <hyk8899@mail.notes.bank-of-china.com>
2024-06-19 14:17:49 +0800

    新增流程属性页面中表格和筛选搜索项

--- src/pages/flowAttr/FlowAttr.tsx
+++ src/pages/flowAttr/FlowAttr.tsx
@@ -1,33 +1,128 @@
 import React from 'react';
-import { Space, Table, Tag } from 'antd';
+import { Space, Table, Tag, Input, Form, Row, Button, Select, Col, theme, FormInstance } from 'antd';
 import type { TableProps } from 'antd';
 import './FlowAttr.scss'
 
 function FlowAttr () {
+  interface OptionType {
+    value: string;
+    text: string;
+  }
+  interface ItemType {
+    name: string;
+    label: string;
+    type: string;
+    options?: OptionType[];
+  }
+
+  const { Option } = Select;
+  const { token } = theme.useToken();
+  const [form] = Form.useForm();
+
+  const formStyle: React.CSSProperties = {
+    maxWidth: 'none',
+    background: token.colorFillAlter,
+    borderRadius: token.borderRadiusLG,
+    padding: 24,
+  };
+
+  const searchList = [
+    {
+      name:'flowId',
+      label:'流程Id',
+      type: 'input',
+    },
+    {
+      name:'flowName',
+      label:'流程名',
+      type: 'input',
+    },
+    {
+      name:'status',
+      label:'状态',
+      type: 'select',
+      options: [
+        {
+          text: '全部',
+          value: '0'
+        },
+        {
+          text: 'nice',
+          value: '1'
+        },
+        {
+          text: 'loser',
+          value: '2'
+        },
+        {
+          text: 'cool',
+          value: '3'
+        },
+      ]
+    }
+  ]
+
+  const getFormItem = (item: ItemType, index: number) => {
+    switch (item.type) {
+      case 'select':
+        return (
+          <Form.Item
+            name={item.name}
+            label={item.label + ':'}
+            initialValue="0"
+          >
+            <Select>
+              {item.options && item.options.map((option, optionIndex) => (
+                <Option key={optionIndex} value={option.value}>
+                  {option.text}
+                </Option>
+              ))}
+            </Select>
+          </Form.Item>
+        );
+      default:
+        return (  
+          <Form.Item
+            name={item.name}
+            label={item.label + ':'}
+          >
+            <Input placeholder={'请输入' + item.label} />
+          </Form.Item>
+        );
+    }
+  }
+
+  const getFields = () => {
+    const children = [];
+    for (let i = 0; i < searchList.length; i++) {
+      const item = searchList[i]
+      children.push(<Col span={8} key={i}>{getFormItem(item, i)}</Col>);
+    }
+    return children;
+  };
+  const onFinish = (values: any) => {
+    console.log('Received values of form: ', values);
+  };
+  const resetForm = (form: FormInstance) => {
+    form.resetFields();
+  };
   interface DataType {
     key: string;
-    name: string;
-    age: number;
-    address: string;
+    flowId: string;
+    flowName: string;
     status: string;
   }
   
   const columns: TableProps<DataType>['columns'] = [
     {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-      render: (text: string) => <span>{text}</span>,
+      title: '流程Id',
+      dataIndex: 'flowId',
+      key: 'flowId',
     },
     {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
-    },
-    {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address',
+      title: '流程名',
+      dataIndex: 'flowName',
+      key: 'flowName',
     },
     {
       title: '状态',
@@ -55,7 +150,7 @@ function FlowAttr () {
       key: 'action',
       render: (_, record) => (
         <Space size="middle">
-          <span>Invite {record.name}</span>
+          <span>Invite {record.flowName}</span>
           <span>Delete</span>
         </Space>
       ),
@@ -65,32 +160,37 @@ function FlowAttr () {
   const data: DataType[] = [
     {
       key: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park',
+      flowId: '32',
+      flowName: 'John Brown',
       status: 'nice',
     },
     {
       key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park',
+      flowId: '42',
+      flowName: 'Jim Green',
       status: 'loser',
     },
     {
       key: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sydney No. 1 Lake Park',
+      flowId: '32',
+      flowName: 'Joe Black',
       status: 'cool',
     },
   ];
 
   return (
     <div>
-      {/* 其他页面内容 */}
-      <div >流程属性列表</div>
-      <Table columns={columns} dataSource={data} />
+      <div className='page-title'>流程属性列表</div>
+      <Form form={form} name="advanced_search" style={formStyle} onFinish={onFinish}>
+        <Row gutter={24}>{getFields()}</Row>
+        <div style={{ textAlign: 'right' }}>
+          <Space size="small">
+            <Button type="primary" htmlType="submit"> 筛选 </Button>
+            <Button onClick={() => {resetForm(form)}}> 重置 </Button>
+          </Space>
+        </div>
+      </Form>
+      <Table bordered columns={columns} dataSource={data} />
     </div>
   );
 }
